22580047397b4c95f8a0cf4df5202155cfd4d01b
Cecco <138607244+cecco25@users.noreply.github.com>
2026-04-13 14:28:08 +0000

    refactor: add enums for colors and moves
    Co-authored-by: francescopausellii <francescopauselli03@gmail.com>

--- uniform_coloring.py
+++ uniform_coloring.py
@@ -1,4 +1,23 @@
+from enum import Enum
 from aima.search import Problem
+
+
+class Color(Enum):
+    BLUE = ('B', 1)
+    YELLOW = ('Y', 2)
+    GREEN = ('G', 3)
+    EMPTY = ('U', 0)  # Placeholder for the initial position of the head (T)
+
+    def __init__(self, symbol, cost):
+        self.symbol = symbol
+        self.cost = cost
+
+
+class Move(Enum):
+    NORTH = (-1, 0)
+    SOUTH = (1, 0)
+    EAST = (0, 1)
+    WEST = (0, -1)
 
 
 class UniformColoring(Problem):
@@ -15,17 +34,21 @@ class UniformColoring(Problem):
         self.cols = len(grid_matrix[0])
         self.start_pos = None
 
+        # Create a mapping from symbols to colors
+        symbol_to_color = {c.symbol: c for c in Color if c != Color.EMPTY}
+
         # Build the initial grid and find the head (T)
         initial_grid = []
         for r in range(self.rows):
             row_colors = []
             for c in range(self.cols):
+                char = grid_matrix[r][c]
                 if grid_matrix[r][c] == 'T':
                     self.start_pos = (r, c)
                     # 'U' = Uncolored (starting position is not colored)
-                    row_colors.append('U')
+                    row_colors.append(Color.EMPTY)
                 else:
-                    row_colors.append(grid_matrix[r][c])
+                    row_colors.append(symbol_to_color[char])
             initial_grid.append(tuple(row_colors))
 
         # The state must be immutable: (colored_grid, head_position)
@@ -40,21 +63,18 @@ class UniformColoring(Problem):
         possible_actions = []
 
         # 1. Actions (N, S, E, W) without leaving the grid
-        if r > 0:
-            possible_actions.append('Nord')
-        if r < self.rows - 1:
-            possible_actions.append('Sud')
-        if c < self.cols - 1:
-            possible_actions.append('East')
-        if c > 0:
-            possible_actions.append('West')
+        for move in Move:
+            dr, dc = move.value
+            new_r, new_c = r + dr, c + dc
+            if 0 <= new_r < self.rows and 0 <= new_c < self.cols:
+                possible_actions.append(move)
 
         # 2. Actions for coloring (col-B, col-Y, col-G)
         # It makes sense to color only if the cell has a different color from the one we want to apply
         current_color = grid[r][c]
-        for color in ['B', 'Y', 'G']:
+        for color in [Color.BLUE, Color.YELLOW, Color.GREEN]:
             if current_color != color:
-                possible_actions.append(f'col-{color}')
+                possible_actions.append(color)
 
         return possible_actions
 
@@ -64,35 +84,24 @@ class UniformColoring(Problem):
         r, c = pos
 
         # Resolution of movements
-        if action == 'Nord':
-            return (grid, (r - 1, c))
-        if action == 'Sud':
-            return (grid, (r + 1, c))
-        if action == 'East':
-            return (grid, (r, c + 1))
-        if action == 'West':
-            return (grid, (r, c - 1))
+        if isinstance(action, Move):
+            dr, dc = action.value
+            return (grid, (pos[0] + dr, pos[1] + dc))
 
         # Resolution of coloring
-        if action.startswith('col-'):
-            new_color = action.split('-')[1]
+        if isinstance(action, Color):
             # Convert the grid to a mutable structure (list of lists) to modify it
-            new_grid = list(list(row) for row in grid)
-            new_grid[r][c] = new_color
+            new_grid = [list(row) for row in grid]
+            new_grid[pos[0]][pos[1]] = action
             # Reconvert the grid to an immutable structure (tuple of tuples) for the new state
-            new_grid_tuple = tuple(tuple(row) for row in new_grid)
-            return (new_grid_tuple, pos)
+            return (tuple(tuple(row) for row in new_grid), pos)
 
     def path_cost(self, c, action):
         """Return the cost of the path c + the cost of the action."""
-        if action in ['Nord', 'Sud', 'East', 'West']:
+        if isinstance(action, Move):
             return c + 1
-        if action == 'col-B':
-            return c + 1
-        if action == 'col-Y':
-            return c + 2
-        if action == 'col-G':
-            return c + 3
+        if isinstance(action, Color):
+            return c + action.cost
         return c
 
     def goal_test(self, state):
@@ -104,11 +113,19 @@ class UniformColoring(Problem):
             return False
 
         # 2. All cells must have the same color (ignoring the initial cell where T is located)
-        colors_found = set()
+        first_color = None
         for r in range(self.rows):
             for c in range(self.cols):
-                if (r, c) != self.start_pos:
-                    colors_found.add(grid[r][c])
+                if (r, c) == self.start_pos:
+                    continue
 
-        # If there is only one color in the set (e.g., only 'G'), we have a uniform coloring, otherwise we do not
-        return len(colors_found) == 1
+                cell_color = grid[r][c]
+                if cell_color == Color.EMPTY:
+                    return False  # Not yet colored
+
+                if first_color is None:
+                    first_color = cell_color
+                elif cell_color != first_color:
+                    return False
+        # If there is only one color in the grid, we have a uniform coloring, otherwise we do not
+        return True
