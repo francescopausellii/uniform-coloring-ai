2a86831ce9c4861a09aa1d9a9011fe0188cef4d9
francescopausellii <francescopauselli03@gmail.com>
2026-06-08 16:38:46 +0200

    feat(gridjs): hand-drawn grid drawing tool

--- gridjs/common/draw.js
+++ gridjs/common/draw.js
@@ -1,21 +1,42 @@
 const draw = {};
 
-draw.path = (ctx, path, color = "black") => {
+draw.path = (ctx, path, color = "black", width = 3) => {
+  if (path.length === 0) return;
   ctx.strokeStyle = color;
-  ctx.lineWidth = 3;
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.lineJoin = "round";
   ctx.beginPath();
   ctx.moveTo(...path[0]);
   for (let i = 1; i < path.length; i++) {
     ctx.lineTo(...path[i]);
   }
-  ctx.lineCap = "round";
-  ctx.lineJoin = "round";
   ctx.stroke();
 };
 
-draw.paths = (ctx, paths, color = "black") => {
+draw.paths = (ctx, paths, color = "black", width = 3) => {
   for (let path of paths) {
-    draw.path(ctx, path, color);
+    draw.path(ctx, path, color, width);
+  }
+};
+
+// Linee di griglia: rows x cols celle di lato `cell`, con bordo `margin`.
+draw.grid = (ctx, rows, cols, cell, margin, color = "black", width = 2) => {
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+  for (let r = 0; r <= rows; r++) {
+    const y = margin + r * cell;
+    ctx.beginPath();
+    ctx.moveTo(margin, y);
+    ctx.lineTo(margin + cols * cell, y);
+    ctx.stroke();
+  }
+  for (let c = 0; c <= cols; c++) {
+    const x = margin + c * cell;
+    ctx.beginPath();
+    ctx.moveTo(x, margin);
+    ctx.lineTo(x, margin + rows * cell);
+    ctx.stroke();
   }
 };
 
--- gridjs/js/sketchpad.js
+++ gridjs/js/sketchpad.js
@@ -1,109 +1,110 @@
-class SketchPad {
-  constructor(container, onUpdate = null, size = 400) {
+// GridSketchPad: canvas con griglia rows x cols su cui scrivere a mano.
+// Sfondo bianco + linee scure + tratti scuri => formato atteso dalla
+// pipeline OpenCV (detect_grid_morph).
+class GridSketchPad {
+  constructor(container, opts = {}) {
+    this.rows = opts.rows ?? 4;
+    this.cols = opts.cols ?? 4;
+    this.cell = opts.cell ?? 80;
+    this.margin = opts.margin ?? 20;
+    this.penWidth = opts.penWidth ?? 4;
+    this.showGuide = opts.showGuide ?? true;
+
+    this.width = this.margin * 2 + this.cols * this.cell;
+    this.height = this.margin * 2 + this.rows * this.cell;
+
     this.canvas = document.createElement("canvas");
-    this.canvas.width = size;
-    this.canvas.height = size;
-    this.canvas.style =
-      "background-color: white; box-shadow: 0px 0px 10px 2px black;";
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.canvas.style = "background-color:white; box-shadow:0 0 8px 1px #888; touch-action:none;";
     container.appendChild(this.canvas);
-
-    let lineBreak = document.createElement("br");
-    container.appendChild(lineBreak);
-
-    this.undoBtn = document.createElement("button");
-    this.undoBtn.innerHTML = "UNDO";
-    this.undoBtn.disabled = true;
-    container.appendChild(this.undoBtn);
+    this.ctx = this.canvas.getContext("2d");
 
     this.paths = [];
     this.isDrawing = false;
 
-    this.ctx = this.canvas.getContext("2d");
-
-    this.onUpdate = onUpdate;
-    this.reset();
-
     this.#addEventListeners();
+    this.render();
   }
 
   reset() {
     this.paths = [];
     this.isDrawing = false;
-    this.#render();
+    this.render();
   }
+
+  undo() {
+    this.paths.pop();
+    this.render();
+  }
+
+  render() {
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(0, 0, this.width, this.height);
+    // guida tenue solo a schermo: NON viene esportata
+    if (this.showGuide) {
+      draw.grid(this.ctx, this.rows, this.cols, this.cell, this.margin, "#d8d8d8", 1);
+    }
+    draw.paths(this.ctx, this.paths, "black", this.penWidth);
+  }
+
+  toggleGuide() {
+    this.showGuide = !this.showGuide;
+    this.render();
+  }
+
+  // PNG con solo i tratti disegnati a mano su bianco (griglia inclusa = quella
+  // che disegni tu). La guida grigia viene esclusa dall'export.
+  toDataURL() {
+    const guide = this.showGuide;
+    this.showGuide = false;
+    this.render();
+    const url = this.canvas.toDataURL("image/png");
+    this.showGuide = guide;
+    this.render();
+    return url;
+  }
+
+  #getPos(evt) {
+    const rect = this.canvas.getBoundingClientRect();
+    const src = evt.touches ? evt.touches[0] : evt;
+    return [
+      Math.round(src.clientX - rect.left),
+      Math.round(src.clientY - rect.top),
+    ];
+  }
+
+  #start = (evt) => {
+    evt.preventDefault();
+    this.paths.push([this.#getPos(evt)]);
+    this.isDrawing = true;
+  };
+
+  #move = (evt) => {
+    if (!this.isDrawing) return;
+    evt.preventDefault();
+    this.paths[this.paths.length - 1].push(this.#getPos(evt));
+    this.render();
+  };
+
+  #end = () => {
+    this.isDrawing = false;
+  };
 
   #addEventListeners() {
-    this.canvas.onmousedown = (evt) => {
-      const mouse = this.#getMouse(evt);
-      this.paths.push([mouse]);
-      this.isDrawing = true;
-    };
+    this.canvas.addEventListener("mousedown", this.#start);
+    this.canvas.addEventListener("mousemove", this.#move);
+    document.addEventListener("mouseup", this.#end);
 
-    this.canvas.onmousemove = (evt) => {
-      if (this.isDrawing) {
-        const mouse = this.#getMouse(evt);
-        let lastPath = this.paths[this.paths.length - 1];
-        lastPath.push(mouse);
-        this.#render();
-      }
-    };
+    this.canvas.addEventListener("touchstart", this.#start, { passive: false });
+    this.canvas.addEventListener("touchmove", this.#move, { passive: false });
+    document.addEventListener("touchend", this.#end);
 
-    document.onmouseup = () => {
-      this.isDrawing = false;
-    };
-
-    this.canvas.ontouchstart = (evt) => {
-      let loc = evt.touches[0];
-      this.canvas.onmousedown(loc);
-    };
-
-    this.canvas.ontouchmove = (evt) => {
-      let loc = evt.touches[0];
-      this.canvas.onmousemove(loc);
-    };
-
-    document.ontouchend = () => {
-      this.canvas.onmouseup();
-    };
-
-    this.undoBtn.onclick = () => {
-      this.paths.pop();
-      this.#render();
-    };
-
-    document.onkeydown = (evt) => {
+    document.addEventListener("keydown", (evt) => {
       if (evt.ctrlKey && evt.key === "z") {
         evt.preventDefault();
-        this.paths.pop();
-        this.#render();
+        this.undo();
       }
-    };
+    });
   }
-
-  #render() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    draw.paths(this.ctx, this.paths);
-    if (this.paths.length > 0) {
-      this.undoBtn.disabled = false;
-    } else {
-      this.undoBtn.disabled = true;
-    }
-    if(this.onUpdate){
-      this.onUpdate(this.paths);
-    }
-  }
-
-  triggerUpdate(){
-    if(this.onUpdate){
-      this.onUpdate(this.paths);
-    }
-  }
-
-  #getMouse = (evt) => {
-    const rect = this.canvas.getBoundingClientRect();
-    return [
-      Math.round(evt.clientX - rect.left),
-      Math.round(evt.clientY - rect.top),
-    ];
-  };
 }
